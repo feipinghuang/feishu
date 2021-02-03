4d14bdda6e2fe727c0e003b30408c8205c060625
feipinghuang <feipinghuang@gmail.com>
2021-02-03 12:09:13 +0800

    add user_info api

--- lib/feishu/api/user.rb
+++ lib/feishu/api/user.rb
@@ -2,22 +2,40 @@ module Feishu
   module Api
     module User
       def batch_get_id(mobiles: [], emails: [])
-        get('/user/v1/batch_get_id', query: { mobiles: mobiles, emails: emails })
+        get(
+          '/user/v1/batch_get_id',
+          query: { mobiles: mobiles, emails: emails },
+        )
       end
 
       def batch_get(employee_ids: [], open_ids: [])
-        get('/contact/v1/user/batch_get', query: { employee_ids: employee_ids, open_ids: open_ids })
+        get(
+          '/contact/v1/user/batch_get',
+          query: { employee_ids: employee_ids, open_ids: open_ids },
+        )
       end
 
       def get_user_id_by_mobile(mobile)
-        res = batch_get_id(mobiles:[mobile])
+        res = batch_get_id(mobiles: [mobile])
         res['mobile_users'][mobile].first['user_id']
       end
 
       def get_user_id_by_open_id(open_id)
-        union_id = batch_get(open_ids: [open_id])['user_infos'].first['union_id']
-        res = get('/user/v1/union_id/batch_get/list', query: { union_ids: [union_id] })
+        union_id =
+          batch_get(open_ids: [open_id])['user_infos'].first['union_id']
+        res =
+          get(
+            '/user/v1/union_id/batch_get/list',
+            query: { union_ids: [union_id] },
+          )
         res['user_infos'][union_id]['user_id']
+      end
+
+      def get_user_info(user_id, user_id_type: 'open_id')
+        get(
+          "/contact/v3/users/#{user_id}",
+          query: { user_id_type: user_id_type },
+        )
       end
     end
   end
