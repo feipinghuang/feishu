3bbd9acbb74262a5a265efacd91468ed18f3b6eb
mios <mios426@gmail.com>
2021-12-22 19:57:59 +0800

    add jsapi_ticket

--- lib/feishu/access_token.rb
+++ lib/feishu/access_token.rb
@@ -16,9 +16,14 @@ module Feishu
       Redis.current.get(:app_access_token) || _app_access_token
     end
 
+    def jsapi_ticket
+      Redis.current.get(:jsapi_ticket) || _jsapi_ticket
+    end
+
     def clear_cache
       Redis.current.del(:tenant_access_token)
       Redis.current.del(:app_access_token)
+      Redis.current.del(:jsapi_ticket)
     end
 
     def user_access_token(grant_type: 'authorization_code', code:)
@@ -37,6 +42,18 @@ module Feishu
         '/authen/v1/refresh_access_token',
         headers: {
           "Authorization": "Bearer #{AccessToken.new.app_access_token}",
+          "Content-Type": 'application/json',
+        },
+        body: { grant_type: grant_type, refresh_token: refresh_token }.to_json,
+      )
+    end
+
+    def jsapi_ticket
+      # post('/jssdk/ticket/get')
+      self.class.post(
+        '/jssdk/ticket/get',
+        headers: {
+          "Authorization": "Bearer #{AccessToken.new.tenant_access_token}",
           "Content-Type": 'application/json',
         },
         body: { grant_type: grant_type, refresh_token: refresh_token }.to_json,
@@ -77,6 +94,23 @@ module Feishu
         response['tenant_access_token'],
       )
       response['tenant_access_token']
+    end
+
+    def _jsapi_ticket
+      response =
+        self.class.post(
+          '/jssdk/ticket/get',
+          headers: {
+            "Authorization": "Bearer #{AccessToken.new.tenant_access_token}",
+            "Content-Type": 'application/json',
+          },
+        )
+      Redis.current.setex(
+        :jsapi_ticket,
+        response['data']['expire_in'] - 5,
+        response['data']['ticket'],
+      )
+      response['data']['ticket']
     end
   end
 end
